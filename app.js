9a4fb2eacaa2bbb52542137707f7608b7b3d6a79
Ramtin Khalatbari <versayana@outlook.com>
2019-10-12 11:14:34 +0330

    Init users route

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 
 // Routes
 const indexRoute = require('./routes/index');
+const usersRoute = require('./routes/users');
+
 app.use('/', indexRoute);
+app.use('/users', usersRoute);
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
