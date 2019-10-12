a6d501bce9585fa8003c6a5089ac21d24d025dab
Ramtin Khalatbari <versayana@outlook.com>
2019-10-12 20:23:03 +0330

    Init templates

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Routes
 const indexRoute = require('./routes/index');
@@ -10,5 +13,7 @@ const usersRoute = require('./routes/users');
 
 app.use('/', indexRoute);
 app.use('/users', usersRoute);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
